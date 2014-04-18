8a1370e858ad1fee1f18d2e805fd88f3f2abd2d4
staszekj <Stanislaw.Jagiella@gmail.com>
2014-04-18 22:46:18 +0200

    Test

---
+++ FileWalker.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Staszek on 07/03/14.
+ */
+
+var walker = require('walker'),
+    fs = require('fs'),
+    lineReader = require('line-reader');
+
+
+var logAnalyzer = {
+
+    walk: function (dirs, onFile, onEnd) {
+
+        dirs.forEach(function (dir) {
+                walker(dir)
+                    .on('file', function (entry, stat) {
+                        onFile(dir, entry.substr(dir.length));
+                    })
+                    .on('end', function () {
+                        onEnd();
+                    })
+            }
+        )
+
+    },
+    readFile: function (file, onLine, onEnd) {   
+
+        lineReader.eachLine(file, function(line, last) {
+
+            onLine(line.trim());
+
+            if(last){
+                onEnd();
+            }
+        });
+    }
+    /aa
+}
+//bb
+
+module.exports = logAnalyzer;
+
+
